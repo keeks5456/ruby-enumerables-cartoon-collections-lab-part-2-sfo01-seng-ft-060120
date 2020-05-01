10cc2ff50edefe6ed88eb55dc8210fa1fabcc0b2
Akiko Green <akikogreen08@gmail.com>
2020-05-01 04:25:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.detect do |valid|
-    if valid == valid_calls 
-      return valid
+   
     end
   end
 end
