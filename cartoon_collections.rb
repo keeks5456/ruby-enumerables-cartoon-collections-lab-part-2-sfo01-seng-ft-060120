500f9a1ca61ad76379e58c17df0f1b650540ca4c
Akiko Green <akikogreen08@gmail.com>
2020-05-01 04:07:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ array.map do |squared|
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
+  
 end
 
 def long_planeteer_calls(planeteer_calls)
